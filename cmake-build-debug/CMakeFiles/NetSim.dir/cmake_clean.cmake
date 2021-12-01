ab9183e5481d5eba8fc6ea9568e20fbdb7961d15
Adam Filapek <adamfilapek@student.agh.edu.pl>
2021-12-01 17:23:57 +0100

    create_project_01-12-2021

---
+++ cmake-build-debug/CMakeFiles/NetSim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetSim.dir/main.cpp.obj"
+  "NetSim.exe"
+  "NetSim.exe.manifest"
+  "NetSim.pdb"
+  "libNetSim.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NetSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
